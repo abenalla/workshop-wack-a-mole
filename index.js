96800023bc3f3d2fd30d9638e5dcd2fb70b5443e
unknown <aishaben.18@gmail.com>
2020-09-10 21:31:46 -0400

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-let score = 0;
+
+var score = 0;
+var scoreDisplay = document.getElementById('score');
+var holes = document.getElementsByClassName('hole');
+setInterval(function(){
+        var randomHoleIndex = Math.floor(Math.random()*holes.length);
+        holes[randomHoleIndex].classList.toggle("mole");
+    },200);
+
+var gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent){
+    if(clickEvent.target.matches('.mole')){
+        clickEvent.target.classList.remove('mole');
+        score++;
+        scoreDisplay.innerText = score;
+    }
+});
